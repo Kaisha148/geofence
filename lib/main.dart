11156f08cfbe6cade90d92290a42c8aeec13df4a
Kaisha148 <kaishataro@gmail.com>
2023-06-28 02:27:06 +0900

    0627クリックしたら円出てくるとこまで

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,10 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_map/plugin_api.dart';
+
+//追加
+import 'package:location/location.dart' as setarea;
+
 void main() {
   runApp(MyApp());
 }
@@ -34,12 +38,20 @@ class _MapExampleState extends State<MapExample> {
   int count = 0;
   int tentotenwokuttukeru = -1;
   late LatLng _tappedLocation;
+  bool toTouch = false;
+
+  //add
+  setarea.LocationData? currentLocation;
+  LatLng? tappedPoint;
 
   @override
   void initState() {
     super.initState();
     getCurrentLocation();
     startLocationUpdates();
+
+    //add
+    getLocation();
   }
 
   void getCurrentLocation() async {
@@ -116,6 +128,42 @@ class _MapExampleState extends State<MapExample> {
     });
   }
 
+  //add
+  Future<void> getLocation() async {
+    setarea.Location location = setarea.Location();
+
+    bool _serviceEnabled;
+    setarea.PermissionStatus _permissionGranted;
+    setarea.LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == setarea.PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != setarea.PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationData = await location.getLocation();
+    setState(() {
+      currentLocation = _locationData;
+    });
+  }
+
+  void handleMapTap(TapPosition event,LatLng latlng) {
+    setState(() {
+      tappedPoint = latlng;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -124,33 +172,66 @@ class _MapExampleState extends State<MapExample> {
       ),
       // body: Column(
         body: GestureDetector(
-        // children: [
-        //   Expanded(
+        //children: [
+           //Expanded(
             child: FlutterMap(
               options: MapOptions(
                 center: LatLng(_currentPosition!.latitude,
                     _currentPosition!.longitude),
                 zoom: 15.0,
-                onTap: _handleMapTap,
+                //onTap: _handleMapTap,
+                //change
+                onTap: (dynamic tapPosition, LatLng latLng) {
+                  toTouch = true;
+                  handleMapTap(tapPosition,latLng);
+                }
               ),
               children: [
                 TileLayer(
                   urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
                   subdomains: ['a', 'b', 'c'],
                 ),
+                  MarkerLayer(
+                    markers: _points.map((LatLng point) {
+                      return Marker(
+                        point: point,
+                        builder: (ctx) => Container(
+                          child: const Icon(
+                            Icons.location_on,
+                            color: Colors.red,
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                if (tappedPoint != null)
                 MarkerLayer(
-                  markers: _points.map((LatLng point) {
-                    return Marker(
-                      point: point,
-                      builder: (ctx) => Container(
-                        child: const Icon(
-                          Icons.location_on,
-                          color: Colors.red,
+                  markers: [
+                      Marker(
+                        width: 80.0,
+                        height: 80.0,
+                        point: tappedPoint!,
+                        builder: (ctx) => Container(
+                          child: Icon(
+                            Icons.place,
+                            color: Colors.blue,
+                            size: 40.0,
+                          ),
                         ),
                       ),
-                    );
-                  }).toList(),
-                ),
+                    ],
+                  ),
+                if (tappedPoint != null)
+                CircleLayer(
+                      circles: [
+                        CircleMarker(
+                          point: tappedPoint!,//isetan
+                          radius: 100,
+                          useRadiusInMeter: true,
+                          color: Colors.yellow
+                        ),
+                      ],
+                    ),
                 CircleLayer(
                       circles: [
                         CircleMarker(
@@ -166,23 +247,15 @@ class _MapExampleState extends State<MapExample> {
                     Polyline(
                       points: _points,
                       color:Colors.red,
-                      strokeWidth: 25.0,
+                      strokeWidth: 10.0,
                     )
                   ],
                 ),
-                if (_tappedLocation != null)
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    'Tapped Location: Lat: ${_tappedLocation.latitude}, Lng: ${_tappedLocation.longitude}',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ),
               ],
             ),
-          ),
+          //),
         //],
-      //),
+      ),
     );
   }
 }
